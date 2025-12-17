ef9a5690fa4f445ffdb8f1f5d290c0fcf3e22f3e
evermake <vladeryabkin@gmail.com>
2025-12-18 01:21:07 +0500

    return Events.ts

---
+++ packages/mini-app/src/Events.ts
@@ -0,0 +1,30 @@
+/**
+ * Event types that can be received by the mini app from the client.
+ *
+ * @see https://core.telegram.org/api/bots/webapps#incoming-events-client-to-mini-app
+ */
+export interface IncomingEventMap {
+  back_button_pressed: void
+
+  settings_button_pressed: void
+}
+
+/**
+ * Event types that can be emitted by the mini app to the client.
+ *
+ * @see https://core.telegram.org/api/web-events#event-apis
+ */
+export interface OutgoingEventMap {
+  web_app_setup_back_button: {
+    is_visible: boolean
+  }
+
+  web_app_setup_settings_button: {
+    is_visible: boolean
+  }
+
+  web_app_trigger_haptic_feedback:
+    | { type: 'impact', impact_style: 'light' | 'medium' | 'heavy' | 'rigid' | 'soft' }
+    | { type: 'notification', notification_type: 'error' | 'success' | 'warning' }
+    | { type: 'selection_change' }
+}
