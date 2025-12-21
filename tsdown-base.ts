461e7d3ecbb8befe67a22a953d463cc75fc8222f
evermake <vladeryabkin@gmail.com>
2025-12-21 10:00:31 +0500

    chore: update tsdown config

--- tsdown-base.ts
+++ tsdown-base.ts
@@ -1,10 +1,16 @@
+import * as path from 'node:path'
+import * as url from 'node:url'
 import { defineConfig } from 'tsdown'
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 export default defineConfig({
   entry: [
     './src/index.ts',
   ],
+  external: ['@grom.js/mini-app'],
   outDir: './dist',
+  unbundle: true,
   platform: 'browser',
   format: 'esm',
   target: false,
@@ -13,6 +19,10 @@ export default defineConfig({
   treeshake: false,
   clean: true,
   dts: {
+    enabled: true,
+    dtsInput: false,
+    eager: true,
+    tsconfig: path.join(__dirname, 'tsconfig.lib.json'),
     build: true,
     sourcemap: true,
   },
