2e00c2b6ef5fd0ed6a0bf729643b44c60d57599d
evermake <vladeryabkin@gmail.com>
2025-12-22 11:06:17 +0500

    chore(react-mini-app): reorder exports

--- packages/mini-app-react/src/index.ts
+++ packages/mini-app-react/src/index.ts
@@ -1,13 +1,26 @@
+/* eslint-disable perfectionist/sort-exports */
+
+// mini app
 export type { MiniApp } from './context.ts'
 export { MiniAppContext } from './context.ts'
-export type { UseBackButtonOptions } from './hooks/useBackButton.ts'
-export { useBackButton } from './hooks/useBackButton.ts'
-export type { UseMainButtonOptions } from './hooks/useMainButton.ts'
-export { useMainButton } from './hooks/useMainButton.ts'
 export { useMiniApp } from './hooks/useMiniApp.ts'
-export type { UseSecondaryButtonOptions } from './hooks/useSecondaryButton.ts'
-export { useSecondaryButton } from './hooks/useSecondaryButton.ts'
-export type { UseSettingsButtonOptions } from './hooks/useSettingsButton.ts'
-export { useSettingsButton } from './hooks/useSettingsButton.ts'
+
+// theme
 export type { UseThemeReturn } from './hooks/useTheme.ts'
 export { useTheme } from './hooks/useTheme.ts'
+
+// main button
+export type { UseMainButtonOptions } from './hooks/useMainButton.ts'
+export { useMainButton } from './hooks/useMainButton.ts'
+
+// secondary button
+export type { UseSecondaryButtonOptions } from './hooks/useSecondaryButton.ts'
+export { useSecondaryButton } from './hooks/useSecondaryButton.ts'
+
+// back button
+export type { UseBackButtonOptions } from './hooks/useBackButton.ts'
+export { useBackButton } from './hooks/useBackButton.ts'
+
+// settings button
+export type { UseSettingsButtonOptions } from './hooks/useSettingsButton.ts'
+export { useSettingsButton } from './hooks/useSettingsButton.ts'
